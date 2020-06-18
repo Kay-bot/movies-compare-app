43dae45dbc73d632fcfde058facea15f38b3de30
KAY S <KS.KWANRUAN@GMAIL.com>
2020-06-18 11:01:27 +1000

    search and helper function

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log('Hi there!');
+const fetchData = async (searchTerm) => {
+  const response = await axios.get("http://www.omdbapi.com", {
+    params: {
+      apikey: "f3038864",
+      s: searchTerm,
+    },
+  });
+
+  console.log(response.data);
+};
+
+const input = document.querySelector("input");
+
+const debounce = (func) => {
+  let timeoutId;
+  return (...args) => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+      func.apply(null, args);
+    }, 1000);
+  };
+};
+
+const onInput = (e) => fetchData(e.target.value);
+
+input.addEventListener("input", debounce(onInput));
